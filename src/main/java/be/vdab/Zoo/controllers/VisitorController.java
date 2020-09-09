0c8a570f9a9953ff72add41513fe36850c90bb96
Marc J <john.doe.gmail.com>
2020-09-09 11:12:09 +0200

    Bootstrap CSS werkt niet

--- src/main/java/be/vdab/Zoo/controllers/VisitorController.java
+++ src/main/java/be/vdab/Zoo/controllers/VisitorController.java
@@ -4,6 +4,7 @@ import be.vdab.Zoo.domain.Visitor;
 import be.vdab.Zoo.domain.enums.VisitorType;
 import be.vdab.Zoo.services.VisitorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -16,6 +17,22 @@ public class VisitorController {
     @Autowired
     VisitorService manager = new VisitorService();
 
+/*
+    @Qualifier("empService")
+    @Autowired
+    EmployeeService employeeService;
+
+    @Qualifier("hrService")
+    @Autowired
+    EmployeeService hrService;
+*/
+
+/*
+    @Value(value = "${vdab.cursus}")
+    private String naamCursus;
+
+*/
+
     @RequestMapping(value = "/getAllVisitors", method = RequestMethod.GET)
     public String getAllVisitors(Model model){
         model.addAttribute("visitors",manager.getAllVisitores());
@@ -25,8 +42,17 @@ public class VisitorController {
     @GetMapping("/{name}/delete")
     public ModelAndView delete(@PathVariable("name") String name) {
         manager.deleteByName(name);
+        return new ModelAndView("redirect:/visitor/getAllVisitors");
+    }
+
+    @GetMapping("/{id}/delete")
+    public ModelAndView delete(@PathVariable("id") int id, ModelMap modelMap) {
+//        employeeService.deleteById(id);
+        modelMap.addAttribute("succes", true);
         return new ModelAndView("redirect:/employee/getAllEmployees");
     }
+
+
 
     @GetMapping("/add")
     public ModelAndView showAddView(ModelMap modelMap){
